ee22ff9dd79bdf5746a310779e569e95db330d7f
Shazmaan <shazmaan@buffalo.edu>
2020-10-16 13:02:25 -0400

    #62 #50 chat history and rendering tests complete

---
+++ Tests/test_chat.py
@@ -0,0 +1,93 @@
+# Do NOT modify this file without consulting the team.
+import os
+import pytest
+import bcrypt
+
+# Do not remove this line. You need this to access content from the main directory
+import sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from SayLess import app
+from SayLess.database import *
+from SayLess.helpers import *
+
+from flask import render_template
+
+app.config['SQLALCHEMY_DATABASE_URI'] = get_secret("TestDB")
+app.app_context().push()
+
+db.reflect()
+db.drop_all()
+
+def test_chat():
+    """Make sure chat works."""
+
+    # testing chat get
+    db.create_all()
+
+    password = ("hello123").encode('utf-8')
+    password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    user = User(username="test",first_name="test",last_name="test",email="shazm@gmail.com",password=password)
+    user2 = User(username="test2",first_name="test2",last_name="test2",email="shazm2@gmail.com",password=password)
+
+    current_room = user2.username + user.username
+
+    conv = Conversation(room=current_room)
+    message = Message(sender=user.username, message="hello")
+    message2 = Message(sender=user2.username, message="world")
+
+    conv.message = [message, message2]
+
+    db.session.add(user)
+    db.session.add(user2)
+    db.session.add(conv)
+    db.session.commit()
+
+    client1 = app.test_client()
+    client2 = app.test_client()
+    client3 = app.test_client()
+
+    # login both users to test end to end
+    login("shazm@gmail.com","hello123", client1)
+    login("shazm2@gmail.com","hello123", client2)
+
+    homepage(user2.username, client1)
+    homepage(user.username, client2)
+
+    history = ""
+
+    for message in conv.message:
+        history += message.sender + ': ' + message.message + '\n'
+
+    # test0 : test if chat renders for client1
+    rv = client1.get("/chat/" + current_room)
+    assert rv.status_code == 200
+    assert render_template("chat.html", messages=history, user=user.username).encode('utf-8') in rv.data
+
+    # test1 : test if chat renders for client2
+    rv = client2.get("/chat/" + current_room)
+    assert rv.status_code == 200
+    assert render_template("chat.html", messages=history, user=user2.username).encode('utf-8') in rv.data
+
+    # test2 : test if redirected to login page for invalid sign in
+    rv = client3.get("/chat/" + current_room)
+    assert rv.status_code == 302
+    assert rv.location.endswith("/login")
+
+    db.reflect()
+    db.drop_all()
+
+# helpers for tests
+def login(email, password, client):
+    return client.post('/login', data=dict(
+        email=email,
+        password=password
+    ), follow_redirects=True)
+
+def homepage(username, client):
+    return client.post('/homepage', data=dict(
+        username=username
+    ), follow_redirects=True)
