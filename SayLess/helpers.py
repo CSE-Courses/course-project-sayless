72faa724edb51d67581d98953fa2053563a4ec23
Shazmaan <shazmaan@buffalo.edu>
2020-11-20 00:07:12 -0500

    #120 fixed for deployment.

--- SayLess/helpers.py
+++ SayLess/helpers.py
@@ -4,7 +4,21 @@ import re
 
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, ContentSettings
 from emoji import UNICODE_EMOJI
+
+def create_container_sample(container_name):
+    # Instantiate a new ContainerClient
+    container_client = blob_service_client.get_container_client(container_name)
+
+    try:
+        # Create new container in the service
+        container_client.create_container()
+
+        return True
+    
+    except:
+        return False
 
 def replace(text):
     text = text.replace(">", "&gt;")
@@ -27,6 +41,10 @@ def get_secret(secretName):
     retrieved_secret = client.get_secret(secretName)
 
     return retrieved_secret.value
+
+# Instantiate a new BlobServiceClient using a connection string
+connection_string = get_secret("Image")
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 def is_emoji(s):
     count = 0
